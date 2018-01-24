643b15c8ecf4224b615aaacc09049527b47d9b32
David Clarke <daveyc09164@gmail.com>
2018-01-24 17:11:09 +0000

    Add files via upload
    Adding all pieces instance class that inherit Piece abstract class

---
+++ pieces/Knight.java
@@ -0,0 +1,62 @@
+package pieces;
+
+import game.Colour;
+import game.Type;
+
+public class Knight extends Piece {
+
+	Type type;
+	
+	public Knight(int x, int y, Colour c) {
+		super(x,y, c);
+		this.type = Type.KNIGHT;
+	}
+	
+	public Type getType() {
+		return this.type;
+	}
+	
+	@Override
+	public Colour getColour() {
+		return this.colour;
+	}
+
+
+	@Override
+	public boolean isValidMove(int x, int y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	@Override
+	public int[][] drawPath() {
+		/**
+		 * A king can only move one space at a time, in any direction.
+		 */
+		return null;
+	}
+	
+	public void setColour(Colour newColour) {
+		super.setColour(newColour);
+	}
+	
+	public void captured() {
+		/**
+		 * This is when another piece takes this piece.
+		 * Should be stored away in case pawn instance brings it back.
+		 */
+	}
+	
+	public void hasCaptured(Piece isCaptured) {
+		/**
+		 * This is when this piece has captured another piece.
+		 * Should definitely think about changing the names of these methods.
+		 */
+		
+		
+	}
+	
+	
+	
+}

ec28662f448d0d6eaec4ad4041ba9a06b5bf57e0
David Clarke <daveyc09164@gmail.com>
2018-01-24 17:34:17 +0000

    Added comment to Player constructor

--- game/Player.java
+++ game/Player.java
@@ -9,6 +9,7 @@ public class Player {
 	String playerName;
 	ArrayList<Piece> ar = new ArrayList<Piece>();
 	
+    //Constructor for Player object
 	public Player(String pName, Colour c) {
 		playerName = pName;
 		ar.add(new Bishop(1,1,Colour.BLACK));
